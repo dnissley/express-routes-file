03877ad968068ccded031237b8981c4cea37412e
Dylan Nissley <dylan.nissley@gmail.com>
2020-06-06 19:49:17 -0500

    chore: add standard js

--- index.js
+++ index.js
@@ -6,12 +6,12 @@ const validHttpMethods = new Set(['GET', 'POST', 'PUT', 'DELETE'])
 
 const wrapAsync = (fn) => {
   return (req, res, next) => {
-    const result = fn(req, res, next);
+    const result = fn(req, res, next)
 
     if (result.then) {
-      return Promise.resolve(result).catch(next);
+      return Promise.resolve(result).catch(next)
     } else {
-      return result;
+      return result
     }
   }
 }
@@ -20,7 +20,7 @@ module.exports = (routeHandlers) => {
   const router = express.Router()
   const data = fs.readFileSync('./routes', 'utf8')
   const lines = data.split(EOL)
-  routes = lines.forEach((line, i) => {
+  lines.forEach((line, i) => {
     // 1 - clean up the string: remove comments, trim whitespace
     let evolvingLine = line
     if (evolvingLine.includes('#')) {
@@ -32,11 +32,11 @@ module.exports = (routeHandlers) => {
     // 2 - split the array by tabs and spaces into an array of route components, getting rid of empty substrings
     const components = cleanLine.split('\t').map(piece => piece.split(' ')).flat().filter(_ => _)
     if (components.length !== 3) {
-      throw new Error(`line ${i+1} could not be parsed into a route: "${line}"`)
+      throw new Error(`line ${i + 1} could not be parsed into a route: "${line}"`)
     }
     const [httpMethod, routePath, routeHandler] = components
     if (!validHttpMethods.has(httpMethod)) {
-      throw new Error(`line ${i+1} specifies an invalid http method: ${httpMethod}`)
+      throw new Error(`line ${i + 1} specifies an invalid http method: ${httpMethod}`)
     }
 
     // 3 - turn the components into an express route
@@ -45,4 +45,3 @@ module.exports = (routeHandlers) => {
 
   return router
 }
-
