779712cd1861144d44119ae74aeaaf7dd9d79858
Dylan Nissley <dylan.nissley@gmail.com>
2020-06-15 23:10:17 -0500

    chore: improve tests

--- test/configureRoutes.test.ts
+++ test/configureRoutes.test.ts
@@ -1,9 +1,11 @@
 import { writeFileSync, existsSync, unlinkSync } from 'fs'
-import express from 'express'
+import express, { Handler } from 'express'
 import request from 'supertest'
 import configureRoutes from '../src/configureRoutes'
 
 const routesFilePath = './routes'
+const testResponseBody = 'this is fine'
+const testResponseHandler: Handler = (req, res) => res.status(200).send(testResponseBody)
 
 const writeRoutesFile = (contents: string) => {
   writeFileSync(routesFilePath, contents)
@@ -20,22 +22,39 @@ describe('configureRoutes()', () => {
     clearRoutesFile()
   })
 
-  it('should set up a basic route', async () => {
+  it('should set up routes', async () => {
     writeRoutesFile(`
-      GET    /test    testHandler
+      GET      /testGet      getHandler
+      POST     /testPost     postHandler
+      PUT      /testPut      putHandler
+      DELETE   /testDelete   deleteHandler
     `)
 
-    const testResponseBody = 'this is fine'
-    const testHandler = jest.fn((req, res) => res.status(200).send(testResponseBody))
     const app = express()
-    const routes = configureRoutes({ testHandler })
+    const handlers: { [key: string]: Handler } = {
+      getHandler: jest.fn(testResponseHandler),
+      postHandler: jest.fn(testResponseHandler),
+      putHandler: jest.fn(testResponseHandler),
+      deleteHandler: jest.fn(testResponseHandler)
+    }
+    const routes = configureRoutes(handlers)
     app.use('/', routes)
 
-    const response = await request(app).get('/test')
+    const responses = [
+      await request(app).get('/testGet'),
+      await request(app).post('/testPost'),
+      await request(app).put('/testPut'),
+      await request(app).delete('/testDelete')
+    ]
 
-    expect(response.status).toBe(200)
-    expect(response.text).toBe(testResponseBody)
-    expect(testHandler).toHaveBeenCalledTimes(1)
+    for (const response of responses) {
+      expect(response.status).toBe(200)
+      expect(response.text).toBe(testResponseBody)
+    }
+
+    for (const handler in handlers) {
+      expect(handlers[handler]).toHaveBeenCalledTimes(1)
+    }
   })
 
   it('should ignore comments and empty lines', async () => {
@@ -47,8 +66,7 @@ describe('configureRoutes()', () => {
       # GET /test thisIsCommentedOutSoWontWork
     `)
 
-    const testResponseBody = 'this is fine'
-    const testHandler = jest.fn((req, res) => res.status(200).send(testResponseBody))
+    const testHandler = jest.fn(testResponseHandler)
     const app = express()
     const routes = configureRoutes({ testHandler })
     app.use('/', routes)
@@ -59,4 +77,35 @@ describe('configureRoutes()', () => {
     expect(response.text).toBe(testResponseBody)
     expect(testHandler).toHaveBeenCalledTimes(1)
   })
+
+  it('should throw an error for routes with too many args', () => {
+    writeRoutesFile(`
+      GET    /test    testHandler    thisShouldCauseAnError
+    `)
+
+    const testHandler = jest.fn(testResponseHandler)
+    expect(() => configureRoutes({ testHandler })).toThrow()
+  })
+
+  it('should throw an error for routes with too few args', () => {
+    writeRoutesFile(`
+      GET    /test
+    `)
+
+    const testHandler = jest.fn(testResponseHandler)
+    expect(() => configureRoutes({ testHandler })).toThrow()
+  })
+
+  it('should throw an error for routes with an invalid http method', () => {
+    writeRoutesFile(`
+      WRONG    /test    testHandler
+    `)
+
+    const testHandler = jest.fn(testResponseHandler)
+    expect(() => configureRoutes({ testHandler })).toThrow()
+  })
+
+  it.todo('should not mess with the flow of exceptions getting passed to next')
+
+  it.todo('should wrap promise rejections and pass them to next')
 })
